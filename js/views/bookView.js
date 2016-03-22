454e6dcb3447288894dc01e0b4c7d23b485fc5b0
smorueta <saramorueta@gmail.com>
2016-03-22 15:04:31 +0000

    FIX error when image links array is empty

--- js/views/bookView.js
+++ js/views/bookView.js
@@ -6,8 +6,11 @@ define(function(){
 			html = '';
 			
 		for (var item in data) {
-			html += "<li class=\"collection-item\"><div class=\"description\"><h2 class=\"title\">" + data[item].title + "</h2><p class=\"paragraph\">"+ data[item].description +"</p></div>" + 
-			"<figure class=\"crop-box-image thumbnail\"><img src=\""+ data[item].imageLinks.thumbnail +"\" alt=\"kkkk\"></figure></li>";
+			if (data[item].hasOwnProperty('imageLinks')) {
+				html += "<li class=\"collection-item\"><div class=\"description\"><h2 class=\"title\">" + data[item].title + "</h2><p class=\"paragraph\">"+ data[item].description +"</p></div>" + 
+				"<figure class=\"crop-box-image thumbnail\"><img src=\""+ data[item].imageLinks.thumbnail +"\" alt=\"kkkk\"></figure></li>";
+			}
+			
 		}
 
 		appDiv.innerHTML = "<ul class=\"collection\">" + html + "</ul>";
